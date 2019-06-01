d6b665eb76cfcc3a9bf0c22dc6947149eb9a11a9
Nuxii <nuxii09@gmail.com>
2019-06-01 02:07:10 +0100

    Added function for better code reuse.

--- src/MediaPlayerPlaylist.vb
+++ src/MediaPlayerPlaylist.vb
@@ -9,23 +9,29 @@ Module MediaPlayerPlaylist
 
     Private Const MPC_HEADER As String = "MPCPLAYLIST"
 
-    Public Function LoadFormatMPCPL(Byval filepath As String) As String()
+    Public Function LoadFormat(ByVal filepath As String) As String()
         If (Not My.Computer.FileSystem.FileExists(filepath)) Then Throw New ArgumentException("Illegal filepath.")
         Dim input As StreamReader = My.Computer.FileSystem.OpenTextFileReader(filepath)
-        If input.EndOfStream Then Throw New ArgumentException("File cannot be empty.")
-        If (input.ReadLine() <> MPC_HEADER) Then Throw New ArgumentException("Invalid file format.")
-        '' compile paths
-        Dim paths As ArrayList = New ArrayList()
-        While (Not input.EndOfStream)
-            Dim record As String() = input.ReadLine().Split(","c)
-            If (record.Length <> 3) Then Throw New ArgumentException("Malformed file structure.")
-            If (record(1) = "filename") Then
-                paths.Add(2)
-            End If
-        End While
+        Dim output As String() = {}
+        LoadFormatMPCPL(input, output)
         input.Close()
         input.Dispose()
-        Return paths.ToArray()
+        Return output
     End Function
+
+    Private Sub LoadFormatMPCPL(Byref stream As StreamReader, ByRef paths As String())
+        If stream.EndOfStream Then Throw New ArgumentException("File cannot be empty.")
+        If (stream.ReadLine() <> MPC_HEADER) Then Throw New ArgumentException("Invalid file format.")
+        '' compile paths
+        Dim pathList As List(Of String) = New List(Of String)
+        While (Not stream.EndOfStream)
+            Dim record As String() = stream.ReadLine().Split(","c)
+            If (record.Length <> 3) Then Throw New ArgumentException("Malformed file structure.")
+            If (record(1) = "filename") Then
+                pathList.Add(record(2))
+            End If
+        End While
+        paths = pathList.ToArray()
+    End Sub
 
 End Module
--- src/PlaylistManager.vb
+++ src/PlaylistManager.vb
@@ -1,7 +1,12 @@
-﻿Module PlaylistManager
+﻿Imports mpc_playlist.MediaPlayerPlaylist
+
+Module PlaylistManager
 
     Sub Main()
-
+        For Each file In MediaPlayerPlaylist.LoadFormat(Console.ReadLine())
+            Console.WriteLine(file)
+        Next
+        Console.ReadKey()
     End Sub
 
 End Module
