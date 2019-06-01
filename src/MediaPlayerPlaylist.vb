fa9916281cfe8c2522300806e29cb267045e08ff
Nuxii <nuxii09@gmail.com>
2019-06-01 04:28:51 +0100

    Added function for loading .pls files.

--- src/MediaPlayerPlaylist.vb
+++ src/MediaPlayerPlaylist.vb
@@ -7,7 +7,8 @@ Imports System.IO
 ''' </summary>
 Module MediaPlayerPlaylist
 
-    Private Const MPC_HEADER As String = "MPCPLAYLIST"
+    Private Const MPCPL_HEADER As String = "MPCPLAYLIST"
+    Private Const PLS_HEADER As String = "[playlist]"
 
     ''' <summary>
     ''' Maintains a relation between file extensions and their decoder function.
@@ -26,6 +27,7 @@ Module MediaPlayerPlaylist
     ''' </summary>
     Sub New()
         fileExtensions.Add(".mpcpl", AddressOf LoadFormatMPCPL)
+        fileExtensions.Add(".pls", AddressOf LoadFormatPLS)
     End Sub
 
     ''' <summary>
@@ -33,7 +35,7 @@ Module MediaPlayerPlaylist
     ''' </summary>
     ''' <param name="filepath">The path of the playlist file.</param>
     ''' <returns>A <c>String()</c> of filepaths.</returns>
-    ''' ''' <exception cref="IOException">Thrown when there was an error loading the file contents.</exception>
+    ''' <exception cref="IOException">Thrown when there was an error loading the file contents.</exception>
     Public Function LoadFormat(ByVal filepath As String) As String()
         If (Not My.Computer.FileSystem.FileExists(filepath)) Then Throw New IOException("Illegal filepath.")
         Dim input As StreamReader = My.Computer.FileSystem.OpenTextFileReader(filepath)
@@ -53,12 +55,31 @@ Module MediaPlayerPlaylist
     ''' </summary>
     ''' <exception cref="IOException">Thrown when there was an error loading the file contents.</exception>
     Private Sub LoadFormatMPCPL(Byref stream As StreamReader, ByRef paths As List(Of String))
-        If (stream.ReadLine() <> MPC_HEADER) Then Throw New IOException("Invalid file format.")
+        If (stream.ReadLine().Trim(" ") <> MPCPL_HEADER) Then Throw New IOException("Invalid file format.")
         While (Not stream.EndOfStream)
             Dim record As String() = stream.ReadLine().Split(","c)
             If (record.Length <> 3) Then Throw New IOException("Malformed file structure.")
             If (record(1) = "filename") Then
                 paths.Add(record(2))
+            End If
+        End While
+    End Sub
+
+    ''' <summary>
+    ''' <see cref="ExtensionPtr"/>
+    ''' </summary>
+    ''' <exception cref="IOException">Thrown when there was an error loading the file contents.</exception>
+    Private Sub LoadFormatPLS(Byref stream As StreamReader, ByRef paths As List(Of String))
+        While (Not stream.EndOfStream)
+            If (stream.ReadLine().Trim(" ") = PLS_HEADER) Then GoTo decode
+        End While
+        Throw New IOException("Invalid file format.")
+        decode:
+        While (Not stream.EndOfStream)
+            Dim record As String() = stream.ReadLine().Split("="c)
+            If (record.Length <> 2) Then Throw New IOException("Malformed file structure.")
+            If (record(0) Like "File*") Then
+                paths.Add(record(1))
             End If
         End While
     End Sub
