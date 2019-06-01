a9b52c75b5b27f5235b7b8ff7a48591ef1b6fe6d
Nuxii <nuxii09@gmail.com>
2019-06-01 02:28:34 +0100

    Added way of easily adding new extensions proceedurally.

--- src/MediaPlayerPlaylist.vb
+++ src/MediaPlayerPlaylist.vb
@@ -8,30 +8,36 @@ Imports System.IO
 Module MediaPlayerPlaylist
 
     Private Const MPC_HEADER As String = "MPCPLAYLIST"
+    Private fileExtensions As Dictionary(Of String, ExtensionPtr) = New Dictionary(Of String, ExtensionPtr)
+    Private Delegate Sub ExtensionPtr(ByRef stream As StreamReader, ByRef paths As List(Of String))
+
+    Sub New()
+        fileExtensions.Add(".mpcpl", AddressOf LoadFormatMPCPL)
+    End Sub
 
     Public Function LoadFormat(ByVal filepath As String) As String()
         If (Not My.Computer.FileSystem.FileExists(filepath)) Then Throw New ArgumentException("Illegal filepath.")
         Dim input As StreamReader = My.Computer.FileSystem.OpenTextFileReader(filepath)
-        Dim output As String() = {}
-        LoadFormatMPCPL(input, output)
+        If input.EndOfStream Then Throw New ArgumentException("File cannot be empty.")
+        '' compile paths
+        Dim paths As List(Of String) = New List(Of String)
+        Dim ext As String = Path.GetExtension(filepath)
+        If (Not fileExtensions.ContainsKey(ext)) Then Throw New ArgumentException("Unknown file extension '" & ext & "'.")
+        fileExtensions(ext)(input, paths)
         input.Close()
         input.Dispose()
-        Return output
+        Return paths.ToArray()
     End Function
 
-    Private Sub LoadFormatMPCPL(Byref stream As StreamReader, ByRef paths As String())
-        If stream.EndOfStream Then Throw New ArgumentException("File cannot be empty.")
+    Private Sub LoadFormatMPCPL(Byref stream As StreamReader, ByRef paths As List(Of String))
         If (stream.ReadLine() <> MPC_HEADER) Then Throw New ArgumentException("Invalid file format.")
-        '' compile paths
-        Dim pathList As List(Of String) = New List(Of String)
         While (Not stream.EndOfStream)
             Dim record As String() = stream.ReadLine().Split(","c)
             If (record.Length <> 3) Then Throw New ArgumentException("Malformed file structure.")
             If (record(1) = "filename") Then
-                pathList.Add(record(2))
+                paths.Add(record(2))
             End If
         End While
-        paths = pathList.ToArray()
     End Sub
 
 End Module
