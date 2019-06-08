d42fc92d6c65180c6c14004972c59d181c22cc43
Nuxii <nuxii09@gmail.com>
2019-06-08 01:14:02 +0100

    Added PLS parser.

--- src/PlaylistParsers/PLS.vb
+++ src/PlaylistParsers/PLS.vb
@@ -3,12 +3,12 @@ Imports System.IO.StreamWriter
 Imports System.IO
 
 ''' <summary>
-''' Decodes and encodes .mpcpl files.
+''' Decodes and encodes .pls files.
 ''' </summary>
-Public Class MPCPL
+Public Class PLS
     Implements Playlist.Extension
 
-    Private Const HEADER As String = "MPCPLAYLIST"
+    Private Const HEADER As String = "[playlist]"
 
     ''' <summary>
     ''' <see cref="Playlist.Extension.Decode(ByRef StreamReader)"/>
@@ -16,15 +16,15 @@ Public Class MPCPL
     ''' <exception cref="IOException">Thrown if there was a problem decoding the file.</exception>
     Public Function Decode(ByRef stream As StreamReader) As String() Implements Playlist.Extension.Decode
         While (Not stream.EndOfStream)
-            If (stream.ReadLine().Trim(" ") <> HEADER) Then GoTo decode
+            If (stream.ReadLine().Trim(" ") = HEADER) Then GoTo decode
         End While
         Throw New IOException("Missing playlist header.")
         decode:
         Dim paths As List(Of String) = New List(Of String)
         While (Not stream.EndOfStream)
-            Dim record As String() = stream.ReadLine().Split(","c)
-            If (record.Length <> 3) Then Throw New IOException("Malformed file structure.")
-            If (record(1) = "filename") Then paths.Add(record(2))
+            Dim record As String() = stream.ReadLine().Split("="c)
+            If (record.Length <> 2) Then Throw New IOException("Malformed file structure.")
+            If (record(0) Like "File*") Then paths.Add(record(1))
         End While
         Return paths.ToArray()
     End Function
@@ -36,10 +36,11 @@ Public Class MPCPL
         stream.WriteLine(HEADER)
         Dim i As Integer = 1
         For Each path In paths
-            stream.WriteLine(i & ",type,0")
-            stream.WriteLine(i & ",filename," & path)
+            stream.WriteLine("File" & i & "=" & path)
             i += 1
         Next
+        stream.WriteLine("NumberOfEntries=" & i)
+        stream.WriteLine("Version=3")
     End Sub
 
 End Class
