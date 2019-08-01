e639e861a5c843b342e7ad0e698fb6b32a03ac6d
Nuxii <nuxii09@gmail.com>
2019-08-01 19:54:22 +0100

    Updated main.

--- src/Main.vb
+++ src/Main.vb
@@ -1,31 +1,24 @@
-﻿Imports mpc_playlist.Playlist
-Imports mpc_playlist.Command
+﻿Imports mpc_playlist.Command
 
+''' <summary>
+''' Main module, baby!!!!!!
+''' </summary>
 Module Main
 
+    ''' <summary>
+    ''' Main method, baby!!!!!!!!!!
+    ''' </summary>
     Sub Main()
-        
-        Command.Parse("help")
-
-        Dim audio As Id3.Mp3 = New Id3.Mp3(Console.ReadLine().Trim(""""c))
-        for each tag in audio.GetAllTags()
-            dim artists As String = tag.Artists
-            dim genre As String = tag.Genre
-            Console.WriteLine("Arist(s): " & artists)
-            Console.WriteLine("Genre(s):" & genre)
-        Next
-        Console.WriteLine("ok")
-
-        Dim pls As Playlist = New Playlist()
-
-        pls.Load(Console.ReadLine().Trim(""""c))
-
-        For each path In pls
-            Console.WriteLine(path)
-        Next
-
-        Console.ReadKey()
-
+        Dim args As List(Of String) = New List(Of String)(Environment.GetCommandLineArgs())
+        args.RemoveAt(0) '' remove the first argument, which is usually the path to the executable
+        Try 
+            Command.Parse(args.toArray())
+        Catch e As Exception
+            Console.ForegroundColor = ConsoleColor.DarkRed
+            Console.Write("ERROR")
+            Console.ResetColor()
+            Console.WriteLine(": " & e.Message)
+        End Try
     End Sub
 
 End Module
