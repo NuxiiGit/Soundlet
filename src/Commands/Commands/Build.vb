fcc107ca6671b18f8497adbfa2a85fca5927f637
Nuxii <nuxii09@gmail.com>
2019-08-02 16:19:02 +0100

    Updated build command help information.

--- src/Commands/Commands/Build.vb
+++ src/Commands/Commands/Build.vb
@@ -8,7 +8,12 @@ Public Class Build
     Public ReadOnly Property Description As String Implements Extension.Description
         Get
             Return "Builds a new playlist from a directory of files." & _
-                    Environment.NewLine & Environment.NewLine & "The full syntax of this command is: 'build <destination> [--absolute/relative] [--append] [-mask <mask>] [-rating <amount>] [-genres <genres>] [-artists <artists>] [-album <album>]'"
+                    Environment.NewLine & Environment.NewLine & "The full syntax of this command is: 'build <destination> <directories> [--absolute/relative] [--append] [-mask <mask>] [-rating <amount>] [-genres <genres>] [-artists <artists>] [-album <album>]'" & _
+                    Environment.NewLine & Environment.NewLine & "Use 'build <playlist> <directory> -mask <mask>' to build a playlist containing files inside 'directory' which match the pattern 'mask.' Masks follow the usual FILE MASK syntax." & _
+                    Environment.NewLine & Environment.NewLine & "Use 'build <playlist> <directory> -rating <amount>' to build a playlist where each audio file must exceed a minimum rating of 'amount.'" & _
+                    Environment.NewLine & Environment.NewLine & "Use 'build <playlist> <directory> -genres <genre>' to build a playlist where each audio file is of a certain genre 'genre.'" & _
+                    Environment.NewLine & Environment.NewLine & "Use 'build <playlist> <directory> -artists <artist>' to build a playlist where each audio file contans a certain artist 'artist.'" & _
+                    Environment.NewLine & Environment.NewLine & "Use 'build <playlist> <directory> -album <album>' to build a playlist where each audio file is part of a certain album 'album.'"
         End Get
     End Property
 
