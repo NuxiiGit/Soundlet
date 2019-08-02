9c3047f9136ad1eb5ed929cb735bec3d189ced56
Nuxii <nuxii09@gmail.com>
2019-08-02 20:23:31 +0100

    Updated the way the update command works.

--- src/Commands/Commands/Update.vb
+++ src/Commands/Commands/Update.vb
@@ -7,7 +7,7 @@ Public Class Update
     Public ReadOnly Property Description As String Implements Extension.Description
         Get
             Return "Adds or removes contents of a playlist." & _
-                    Environment.NewLine & Environment.NewLine & "The full syntax of this command is: 'update <playlist> [--absolute/relative] [-add <files>] [-del <files>]'" & _
+                    Environment.NewLine & Environment.NewLine & "The full syntax of this command is: 'update <playlist> [-add <files>] [-del <files>] [--absolute/relative]'" & _
                     Environment.NewLine & Environment.NewLine & "Use 'update <playlist> --absolute' to force all filepaths to be absolute paths. Alternatively, use 'update <playlist> --relative' to make all filepaths become relative to the playlist file; this is useful when there is a strong bond between your playlist files and your audio files." & _
                     Environment.NewLine & Environment.NewLine & "Use 'update <playlist> --add file1 [file2] [...]' to add a list of audio files to the playlist. The same can be done for deletion: use 'update <playlist> --del file1 [file2] [...]' to remove a list of audio files to the playlist."
         End Get
@@ -20,17 +20,10 @@ Public Class Update
         Dim playlist As Playlist = New Playlist(path)
         Dim values As List(Of String) = New List(Of String)(params)
         values.RemoveAt(0)
-        Dim relative As Boolean = False
         Dim attribute As String = Nothing
         For Each value In values
             Select value
-            Case "--absolute":
-                '' make the playlist absolute
-                relative = False
-            Case "--relative":
-                '' make the playlist relative
-                relative = True
-            Case "-add", "-del":
+            Case "-add", "-del", "--absolute", "--relative":
                 attribute = value
             Case Else:
                 Select attribute
@@ -45,7 +38,7 @@ Public Class Update
                 End Select
             End Select
         Next
-        playlist.Save(path, relative)
+        playlist.Save(path, attribute = "--relative")
     End Sub
 
 End Class
