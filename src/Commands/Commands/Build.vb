6ee0cc8f5220b82a0e457ddc035879bc85982d8b
Nuxii <nuxii09@gmail.com>
2019-08-02 01:26:26 +0100

    Created shell of build command.

---
+++ src/Commands/Commands/Build.vb
@@ -0,0 +1,22 @@
+﻿Imports mpc_playlist.Playlist
+Imports mpc_playlist.Command
+Imports Id3
+
+Public Class Build
+    Implements Command.Extension
+
+    Public ReadOnly Property Description As String Implements Extension.Description
+        Get
+            Return "Builds a new playlist from a directory of files." & _
+                    Environment.NewLine & Environment.NewLine & "The full syntax of this command is: 'build <destination> [--absolute/relative] [--append] [-mask <mask>] [-rating <amount>] [-genres <genres>] [-artists <artists>] [-album <album>]'"
+        End Get
+    End Property
+
+    ''' <exception cref="ArgumentException">Thrown when <paramref name="params"/> is empty.</exception>
+    Public Sub Execute(ParamArray params() As String) Implements Extension.Execute
+        If (params.Length = 0) Then Throw New ArgumentException("You must supply a playlist to update the contents of.")
+        Dim path As String = params(0)
+        '' TODO
+    End Sub
+
+End Class
