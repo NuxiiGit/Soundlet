ae7dc772f212f5b6f01377b9b822081adf70f1d7
Nuxii <nuxii09@gmail.com>
2019-10-25 06:04:17 +0100

    Added shuffle command.

---
+++ src/Builder/Commands/Shuffle.vb
@@ -0,0 +1,10 @@
+﻿Imports mpc_playlist.Builder
+
+Public Class Shuffle
+    Implements ICommand
+
+    Public Sub Execute(ByRef list As Playlist, ByVal ParamArray params As String()) Implements ICommand.Execute
+        list.Shuffle()
+    End Sub
+
+End Class
