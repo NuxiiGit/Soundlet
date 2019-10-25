bc137c0b1a984ad0dda74345fc4df80a59e3527e
Nuxii <nuxii09@gmail.com>
2019-10-25 06:01:52 +0100

    Added some basic commands.

---
+++ src/Builder/Commands/Relative.vb
@@ -0,0 +1,10 @@
+﻿Imports mpc_playlist.Builder
+
+Public Class Relative
+    Implements ICommand
+
+    Public Sub Execute(ByRef list As Playlist, ByVal ParamArray params As String()) Implements ICommand.Execute
+        list.relative = True
+    End Sub
+
+End Class
---
+++ src/Builder/Commands/Remove.vb
@@ -0,0 +1,12 @@
+﻿Imports mpc_playlist.Builder
+
+Public Class Remove
+    Implements ICommand
+
+    Public Sub Execute(ByRef list As Playlist, ByVal ParamArray params As String()) Implements ICommand.Execute
+        For Each record As String In params
+            list.Remove(record)
+        Next
+    End Sub
+
+End Class
